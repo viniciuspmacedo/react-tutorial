0114c7e8292b021f3bd08852a76096995b5d24e7
Vinicius Pereira Macedo <83282287+viniciuspmacedo@users.noreply.github.com>
2024-02-02 00:22:30 +0000

    jogo funcionando

---
+++ src/components/Game/index.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Board from "../Board"
+
+export default function Game(){
+
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const currentSquare = history[history.length-1];
+
+    function handlePlay(nextSquares){
+        setHistory([...history, nextSquares]);
+        setXIsNext(!xIsNext);
+    }
+
+    return(
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext={xIsNext} squares ={currentSquare} onPlay = {handlePlay}/>
+            </div>
+            <div className="game-info">
+                <ol>
+
+                </ol>
+            </div>
+        </div>
+    )
+}
